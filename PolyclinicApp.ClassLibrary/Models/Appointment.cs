62dbd59dd13d162af12f7daa173b86d0fa940e65
mayur <mjm090972@gmail.com>
2025-03-05 18:47:58 +0530

    Packages ,Scaffolding, Configuration File, Class Library, Console Application, GetPatientDetails

---
+++ PolyclinicApp.ClassLibrary/Models/Appointment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PolyclinicApp.ClassLibrary;
+
+public partial class Appointment
+{
+    public int AppointmentNo { get; set; }
+
+    public string PatientId { get; set; } = null!;
+
+    public string DoctorId { get; set; } = null!;
+
+    public DateOnly DateofAppointment { get; set; }
+
+    public virtual Doctor Doctor { get; set; } = null!;
+
+    public virtual Patient Patient { get; set; } = null!;
+}
